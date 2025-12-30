95d8928d9fb7d96e8b3b8a426f7088f597000c2b
prashxant <notxdead0@gmail.com>
2025-12-31 01:29:30 +0530

    init

---
+++ app/components/footer.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="m-10 text-center text-zinc-600">
+      <div className="h-px bg-zinc-800 mx-auto max-w-[600px] mb-6" />
+      <p className="text-sm font-bold  md:text-base">
+        © 2026 26labs. All rights reserved.
+      </p>
+      <p className="mt-2 font-bold  text-sm md:text-base text-col-yel">
+        Made with ❤️ by 26labs
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "./components/footer";
+
+
+export default function Home() {
+  return (
+    <div className="w-screen bg-cover h-screen flex flex-col justify-end bg-[url('/26labs.svg')] ">
+
+ <Footer/>
+    </div>
+  );
+}
