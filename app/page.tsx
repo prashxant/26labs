81930e8d98c73a34f5910bc75ca17bf1b1958c74
prashxant <notxdead0@gmail.com>
2026-01-02 23:23:44 +0530

    fevicon bg relodes fast

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex w-screen h-screen justify-center items-center">
-      <Image
-        src="/26labsBg.svg"
-        alt="background"
-        fill
-        className="object-cover max-md:object-top w-full h-full absolute -z-10 opacity-90"
-        priority
-      />
-      <Hero/>
+    <div
+      className="relative flex w-screen h-screen justify-center items-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/26labsBg.svg')" }}
+    >
+      <Hero />
     </div>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 
 
 export const Hero = () => {
   return (
     <div>
-      <h1 className="text-xl sm:text-3xl text-shadow-md md:text-4xl lg:text-5xl font-bold text-gray-700 tracking-tight">
+      <Image className="mx-auto mb-5" loading="lazy" alt="logo" sizes="10" width={200} height={200} src='/logo.svg'/>
+      <h1 className="text-xl sm:text-3xl text-shadow-xl md:text-4xl lg:text-5xl font-bold text-black tracking-tight">
         Good things take time
       </h1>
       <p className="pl-1 text-gray-500  tracking-tighter font-semibold">
