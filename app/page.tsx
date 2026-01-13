f523d3ec4e18fdec553e4eaf59cf6cb1ca12f5b8
prashxant <notxdead0@gmail.com>
2026-01-13 18:40:21 +0530

    random checks

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
       className="relative flex w-screen h-screen justify-center items-center bg-cover bg-center"
       style={{ backgroundImage: "url('/26labsBg.svg')" }}
     >
+      <p></p>
       <Hero />
     </div>
   );
