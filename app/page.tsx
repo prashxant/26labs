78368d5c459c397e6a814b77aada64492a8bf8df
prashxant <notxdead0@gmail.com>
2026-01-17 14:51:44 +0530

    talyor swift

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,38 @@
-import { Container } from "@/components/shared/Container";
+import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="">
-      <Container className=" mt-20">
-        <div></div>
-
-      </Container>
+    <div
+      className="relative flex w-screen h-screen justify-center items-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/26labsBg.svg')" }}
+    >
+      <Hero />
     </div>
   );
 }
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col mx-auto">
+      <Image
+        className="mx-auto mb-5"
+        loading="lazy"
+        alt="logo"
+        sizes="10"
+        width={200}
+        height={200}
+        src="/logo.svg"
+      />
+      <h1 className="text-xl sm:text-3xl text-shadow-xl md:text-4xl lg:text-5xl font-bold text-black tracking-tight">
+        Good things take time
+      </h1>
+      <p className="pl-1 text-gray-500 text-center tracking-tighter font-semibold">
+        we are working on
+        <span className="cursor-pointer px-1 text-rose-700/50">
+          26labs.livessss
+        </span>
+      </p>
+    </div>
+  );
+};
