8bbcdc6ccebb086b26b5e7aaf26a43d84e2ff619
NABADEEP KR. DAS <nabadeepdas069@gmail.com>
2026-01-23 22:36:14 +0530

    working on fueling part

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,16 +5,20 @@ import HorizontalScrollSection from "@/components/sections/HorizontalScroll";
 import { LiveProductPage } from "@/components/shared/Live Product/LiveProductPage";
 import ToolkitPage from "@/components/shared/Toolkit/ToolkitPage";
 import  Brand  from "@/components/shared/Brand Section/Brand";
+import Fueling from "@/components/shared/Fueling Section/Fueling";
 
 
 export default function Home() {
   return (
     <div>
-      <Hero />
+      <Hero /> 
         <ToolkitPage/>
         <LiveProductPage/>
         <Brand/>
            <HorizontalScrollSection />
+           <Fueling/>
     </div>
   );
 }
+
+
