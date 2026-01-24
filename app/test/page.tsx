df937f0924df86708517076d385ac9dac6cb8cac
Prashant Sharma <117459774+prashxant@users.noreply.github.com>
2026-01-24 11:45:58 +0530

    Fix merge conflict in Home component

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -11,11 +11,7 @@ import Fueling from "@/components/shared/Fueling Section/Fueling";
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
-      <Hero />
-=======
-      <Hero /> 
->>>>>>> 5bacc41ca131da80feea2e1a3781751c915fe427
+        <Hero />
         <ToolkitPage/>
         <LiveProductPage/>
         <Brand/>
