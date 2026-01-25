b764eeb8a7d65e12c57ee8e99e178f08d67070ba
NABADEEP KR. DAS <nabadeepdas069@gmail.com>
2026-01-25 20:51:43 +0530

    cpmplete the  growth  part1

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,20 +5,16 @@ import { LiveProductPage } from "@/components/shared/Live Product/LiveProductPag
 import ToolkitPage from "@/components/shared/Toolkit/ToolkitPage";
 import  Brand  from "@/components/shared/Brand Section/Brand";
 import Fueling from "@/components/shared/Fueling Section/Fueling";
-<<<<<<< HEAD
 import { Footer } from "@/components/shared/Footer Section/Footer";
 import Need from "@/components/shared/Need Section/Need";
 import Growth from "@/components/shared/Growth Section/Growth";
-=======
 
 
->>>>>>> e08fa974d61ab1d7757a47d7f5b9375acc3e8de4
 
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <div>
+    <><div>
       <Hero />
       <ToolkitPage />
       <LiveProductPage />
@@ -28,20 +24,17 @@ export default function Home() {
       <Need />
       <Growth />
       <Footer />
-      
-    </div>
-=======
-    <>
-      <main id="main">
-        <Hero />
-        <ToolkitPage />
-        <LiveProductPage />
-        <Brand />
-        <HorizontalScrollSection />
-        <Fueling />
-      </main>
-    </>
->>>>>>> e08fa974d61ab1d7757a47d7f5b9375acc3e8de4
+
+    </div><>
+        <main id="main">
+          <Hero />
+          <ToolkitPage />
+          <LiveProductPage />
+          <Brand />
+          <HorizontalScrollSection />
+          <Fueling />
+        </main>
+      </></>
   );
 }
 
