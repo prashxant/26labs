957523b094dc16ee509a671a259ba20b01cf4b63
prashxant <notxdead0@gmail.com>
2026-02-02 13:48:10 +0530

    svg

--- components/icons/footerSvg.tsx
+++ components/icons/footerSvg.tsx
@@ -1,41 +1,32 @@
-import { cn } from "@/lib/utils";
+import React from "react";
 
-type FooterSvgProps = {
-  className?: string;
-};
-
-export const FooterSvg = ({ className }: FooterSvgProps) => {
+export const FooterSvg = () => {
   return (
     <svg
-      className={cn("w-full mask-x-from-65% mask-r-to-85%", className)}
-      width="1800"
-      height="1600"
-      viewBox="0 0 1800 1600"
+      width="1440"
+      height="1477"
+      viewBox="0 0 1440 1477"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg" >
-      <g filter="url(#filter0_d_732_2)">
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-screen bg-red-200 absolute -z-5000"
+    >
+      <g filter="url(#filter0_d_1006_40)">
         <path
-          d="   M2058.5 52.4551
-            C1458.5 52.4551 1096.04 189.968 954.142 478.365
-            C832.983 724.622 736.362 823.25 187.824 528.622
-            C187.824 538.622 26.7005 395.607 -24.8431 528.622
-            C-88.0365 691.7 164.308 901.776 164.308 901.776
-            C349.199 1038.1 778.897 1247.79 1018.56 1200.006
-          C1258.22 1168.222 1466.04 1178.4 1850 1390     "
-          stroke="url(#paint0_linear_732_2)"
-          strokeWidth="78"
-          strokeLinecap="round"     />
+          d="M1583.49  53.7803C1583.49 53.7803 914.501 224.78 867.5 458.78C820.499 692.78 1335.5 706.391 1101.5 927.28C867.5 1148.17 -286 1414.78 -286 1414.78 1500"
+          stroke="url(#paint0_linear_1006_40)"
+          stroke-width="78"
+        />
       </g>
-
       <defs>
         <filter
-          id="filter0_d_732_2"
-          x="-120"
+          id="filter0_d_1006_40"
+          x="-314.775"
           y="0"
-          width="1900"
-          height="1600"
+          width="1927.93"
+          height="1476.77"
           filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"     >
+          color-interpolation-filters="sRGB"
+        >
           <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
@@ -53,26 +44,25 @@ export const FooterSvg = ({ className }: FooterSvgProps) => {
           <feBlend
             mode="normal"
             in2="BackgroundImageFix"
-            result="effect1_dropShadow_732_2"
+            result="effect1_dropShadow_1006_40"
           />
           <feBlend
             mode="normal"
             in="SourceGraphic"
-            in2="effect1_dropShadow_732_2"
+            in2="effect1_dropShadow_1006_40"
             result="shape"
           />
         </filter>
-
         <linearGradient
-          id="paint0_linear_732_2"
-          x1="900"
-          y1="700"
-          x2="1550"
-          y2="950"
+          id="paint0_linear_1006_40"
+          x1="997"
+          y1="579.78"
+          x2="1470"
+          y2="729.28"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#8CA9FF" />
-          <stop offset="1" stopColor="#E9EBF8" />
+          <stop stop-color="#8CA9FF" />
+          <stop offset="1" stop-color="#E9EBF8" />
         </linearGradient>
       </defs>
     </svg>
