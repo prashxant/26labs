14743885247514195bfe1c530f2dd6c06dee6903
prashxant <notxdead0@gmail.com>
2026-02-03 18:36:57 +0530

    degsin

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,19 @@
 
+import {  UFO } from '@/components/icons/UFO';
 import React from 'react'
 
 function page() {
   return (
     <div className="h-screen w-full text-7xl flex text-shadow-sm  flex-col gap-10 justify-center items-center">
       <p className="text-orange1 ">Coming Soon...</p>
+      <UFO/>
     </div>
   );
 }
 
 export default page
+
+
+
+
+
