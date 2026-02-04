1a8d3be06746dc77c15d05d84ec5c839de5bc9c7
prashxant <notxdead0@gmail.com>
2026-02-04 19:58:00 +0530

    footer

--- components/shared/Footer Section/Slay.tsx
+++ components/shared/Footer Section/Slay.tsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import { Socials } from './Socials'
 import { LetsConnect } from './LetsConnetc'
-import { UFO } from '@/components/icons/UFO'
 
 export const Slay = () => {
   return (
-    <div className="">
-      <div className="flex  justify-between items-center">
+    <div className="w-full  ">
+      <div className="flex justify-between  items-center">
         <div className="relative">
           <LetsConnect />
-          <UFO
-            className="
-              w-full
-              absolute
-              2xl: -translate-x-6
-              2xl:-translate-y-6
-              -z-10
-            "
-          />
         </div>
         <div className="">salex@26labs.live</div>
         <div className="">
