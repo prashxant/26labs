0ecd83f60cd12dc1cd1e4987368624c98c62cfda
prashxant <notxdead0@gmail.com>
2026-02-05 10:53:31 +0530

    chi ui rotoate

--- components/shared/Footer Section/LetsConnetc.tsx
+++ components/shared/Footer Section/LetsConnetc.tsx
@@ -8,7 +8,7 @@ export const LetsConnect = () => {
     <div className="flex flex-col items-center gap-10 sm:items-end sm:flex-row sm:justify-between  relative w-full px-4 sm:px-6">
       <div className="relative leading-[0.9] text-4xl sm:text-5xl md:text-7xl lg:text-[95px]">
         <h1>Let&apos;s</h1>
-        <h1 className="-rotate-2">Connect </h1>
+        <h1 className="">Connect </h1>
       </div>
       <p>sales@26labs.live</p>
 
