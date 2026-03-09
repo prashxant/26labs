0fa15a24a2df2132c310d77deb816ac253ceac3f
prashxant <notxdead0@gmail.com>
2026-03-09 18:07:22 +0530

    coding

--- app/plus/page.tsx
+++ app/plus/page.tsx
@@ -1,10 +1,13 @@
 
+import { HeroTxt } from '@/components/plus/hero/HeroTxt';
+import { NotBackedByYC } from '@/components/plus/hero/NotBackedYC';
 import React from 'react'
 
 function page() {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-black">
-hi
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-black">
+      <NotBackedByYC/>
+      <HeroTxt/>
     </div>
   );
 }
