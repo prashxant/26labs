55b2883f6100dc3268bfd5c088820e98b521eaaa
prashxant <notxdead0@gmail.com>
2026-03-16 12:11:31 +0530

    showCASE

--- app/plus/page.tsx
+++ app/plus/page.tsx
@@ -1,10 +1,11 @@
 
-import { PlusCard } from "@/components/plus/card/PlusCard";
+
+import ProductShowcase from "@/components/plus/ShawCase";
 
 function page() {
   return (
     <div className="w-full flex justify-center items-center h-[200vh] bg-black">
-      <PlusCard/>
+        <ProductShowcase/>
     </div>
   );
 }
